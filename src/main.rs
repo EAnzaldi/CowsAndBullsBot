cc5510eafd3df5da5b924c24790507060b31a0a6
EAnzaldi <elisabettaanzaldi1@gmail.com>
2026-09-09 18:36:08 +0200

    Updated sobmodule and main part 2

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ pub mod c_ffi;
 #[tokio::main]
 async fn main() {
    match dotenvy::dotenv() {
-      Ok(path) => println!("Token retrieved"),
-      Err(e) => println!("Token not found")
+      Ok(path) => println!("Token retrieved."),
+      Err(e) => println!("Token not found.")
    }
-
-   println!("TELOXIDE_TOKEN = {:?}", std::env::var("TELOXIDE_TOKEN"));
 
    println!("Starting command bot...");
    let bot = Bot::from_env();
